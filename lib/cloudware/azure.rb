a6a8d9b814d4b2907fc6fd41e2fb0a371604b1a5
Vaughan Jones <vaughanlewisjones@icloud.com>
2017-11-14 12:46:32 +0000

    Change azure auth

--- lib/cloudware/azure.rb
+++ lib/cloudware/azure.rb
@@ -19,7 +19,7 @@
 # For more information on the Alces Cloudware, please visit:
 # https://github.com/alces-software/cloudware
 #==============================================================================
-require 'azure_mgmt_resources'
+require 'azure_sdk'
 
 module Cloudware
   module Provider
@@ -27,38 +27,20 @@ module Cloudware
       attr_accessor :name
 
       def initialize
-        puts ENV['AZURE_TENANT_ID']
-        if ENV['AZURE_TENANT_ID'].nil?
-          abort("AZURE_TENANT_ID env var not set")
-        end
-        
-        if ENV['AZURE_CLIENT_ID'].nil?
-          abort("AZURE_CLIENT_ID env var not set")
-        end
-        
-        if ENV['AZURE_SUBSCRIPTION_ID'].nil?
-          abort("AZURE_SUBSCRIPTION_ID env var not set")
-        end
-        
-        if ENV['AZURE_CLIENT_SECRET'].nil?
-          abort("AZURE_CLIENT_SECRET env var not set")
-        end
-
         provider = MsRestAzure::ApplicationTokenProvider.new(
-            ENV['AZURE_TENANT_ID'],
-            ENV['AZURE_CLIENT_ID'],
-            ENV['AZURE_CLIENT_SECRET'])
+                   ENV['AZURE_TENANT_ID'],
+                   ENV['AZURE_CLIENT_ID'],
+                   ENV['AZURE_CLIENT_SECRET'])
         credentials = MsRest::TokenCredentials.new(provider)
-
         options = {
-            credentials: credentials,
-            subscription_id: ENV['AZURE_SUBSCRIPTION_ID']
+          credentials: credentials,
+          subscription_id: ENV['AZURE_SUBSCRIPTION_ID']
         }
-
         @client = Azure::Resources::Profiles::Latest::Mgmt::Client.new(options)
       end
       
-      def self.create_domain(name, networkcidr, subnets)
+      def create_domain(name, networkcidr, subnets)
+        @template = File.read(File.expand_path(File.join(__dir__, '../../templates/azure-network-base.json')))
       end
     end
   end
