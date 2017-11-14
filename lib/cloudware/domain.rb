40731891ad69efecd73a9b308607df815d50c193
Vaughan Jones <vaughanlewisjones@icloud.com>
2017-11-14 11:17:36 +0000

    Generic `Domain` handler

---
+++ lib/cloudware/domain.rb
@@ -0,0 +1,42 @@
+#==============================================================================
+# Copyright (C) 2017 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Cloudware.
+#
+# Alces Cloudware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Cloudware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Cloudware, please visit:
+# https://github.com/alces-software/bumblebee
+#==============================================================================
+
+module Cloudware
+  class Domain
+
+    attr_accessor :name, :networkcidr, :provider
+
+    def self.create(name, provider, networkcidr, subnets)
+      case provider
+      when "azure"
+        azure = Cloudware::Provider::Azure.new
+        azure.create_domain(name, networkcidr, subnets)
+      when "gcp"
+        puts "todo"
+      end
+    end
+
+    def self.list
+    end
+
+  end
+end
