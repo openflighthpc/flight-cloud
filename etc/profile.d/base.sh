97c4b7918d0fc484c4acd81456fc7f2b08bda203
william <william.mccumstie@outlook.com>
2018-05-09 12:27:14 +0100

    Move the profile.d code into a separate file
    This makes it easier to see what happens. Also a bash function has been
    used

---
+++ etc/profile.d/base.sh
@@ -0,0 +1,14 @@
+################################################################################
+##
+## Alces Cloudware - Shell configuration
+## Copyright (c) 2018 Alces Software Ltd
+##
+################################################################################
+
+cloud() {
+  ( target='SED_TARGET_DURING_INSTALL'
+    cd $target
+    PATH="$target/opt/ruby/bin/:$path"
+    bin/cloudware "$@"
+  )
+}
