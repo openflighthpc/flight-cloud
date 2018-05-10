05313ce86afabd52a7be0cefb965fd6d5edc3599
william <william.mccumstie@outlook.com>
2018-05-10 12:50:23 +0100

    Move `domain create` into its own file

--- lib/cloudware/cli.rb
+++ lib/cloudware/cli.rb
@@ -56,54 +56,7 @@ module Cloudware
       c.option '--prvsubnetcidr NAME', String, 'Prv subnet CIDR'
       c.option '--mgtsubnetcidr NAME', String, 'Mgt subnet CIDR'
       c.option '--region NAME', String, 'Provider region to create domain in'
-      c.action do |_args, options|
-        begin
-          d = Cloudware::Domain.new
-
-          options.name = ask('Domain identifier: ') if options.name.nil?
-          d.name = options.name.to_s
-
-          options.provider = choose('Provider name?', :aws, :azure, :gcp) if options.provider.nil?
-
-          options.region = ask('Provider region: ') if options.region.nil?
-          d.region = options.region.to_s
-
-          options.networkcidr = ask('Network CIDR: ') if options.networkcidr.nil?
-          d.networkcidr = options.networkcidr.to_s
-
-          options.prvsubnetcidr = ask('Prv subnet CIDR: ') if options.prvsubnetcidr.nil?
-          d.prvsubnetcidr = options.prvsubnetcidr.to_s
-
-          options.mgtsubnetcidr = ask('Mgt subnet CIDR: ') if options.mgtsubnetcidr.nil?
-          d.mgtsubnetcidr = options.mgtsubnetcidr.to_s
-
-          Whirly.start spinner: 'dots2', status: 'Verifying network CIDR is valid'.bold, stop: '[OK]'.green
-          raise("Network CIDR #{options.networkcidr} is not a valid IPV4 address") unless d.valid_cidr?(options.networkcidr.to_s)
-          Whirly.status = 'Verifying prv subnet CIDR is valid'.bold
-          raise("Prv subnet CIDR #{options.prvsubnetcidr} is not valid for network cidr #{options.networkcidr}") unless d.is_valid_subnet_cidr?(options.networkcidr.to_s, options.prvsubnetcidr.to_s)
-          Whirly.status = 'Verifying mgt subnet CIDR is valid'.bold
-          raise("Mgt subnet CIDR #{options.mgtsubnetcidr} is not valid for network cidr #{options.networkcidr}") unless d.is_valid_subnet_cidr?(options.networkcidr.to_s, options.mgtsubnetcidr.to_s)
-          Whirly.stop
-
-          Whirly.start spinner: 'dots2', status: 'Checking domain name is valid'.bold, stop: '[OK]'.green
-          raise("Domain name #{options.name} is not valid") unless d.valid_name?
-          Whirly.stop
-
-          Whirly.start spinner: 'dots2', status: 'Checking domain does not already exist'.bold, stop: '[OK]'.green
-          raise("Domain name #{options.name} already exists") if d.exists?
-          d.provider = options.provider.to_s
-          Whirly.status = 'Verifying provider is valid'.bold
-          raise("Provider #{options.provider} does not exist") unless d.valid_provider?
-          Whirly.stop
-
-          Whirly.start spinner: 'dots2', status: 'Creating new deployment'.bold, stop: '[OK]'.green
-          d.create
-          Whirly.stop
-        rescue RuntimeError => error
-          Cloudware.log.error("Failed while creating domain: #{error.message}")
-          raise error.message
-        end
-      end
+      action(c, Commands::Domain::Create)
     end
 
     command :'domain list' do |c|
