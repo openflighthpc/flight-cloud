2459e801cdddc49502f006ccd5c8cf33ff43284c
william <william.mccumstie@outlook.com>
2018-05-10 14:39:59 +0100

    Rebrand the profile to be `flightconnector`

--- etc/profile.d/base.sh
+++ etc/profile.d/base.sh
@@ -5,11 +5,12 @@
 ##
 ################################################################################
 
-cloud() {
+flightconnector() {
   ( target='SED_TARGET_DURING_INSTALL' && \
     cd $target && \
     PATH="$target/opt/ruby/bin/:$PATH" && \
     bin/cloud "$@"
   )
 }
+alias fc=flightconnector
 
