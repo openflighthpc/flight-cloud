6b7fff755fe14f5ba4b36d2035f9536bec61a71c
william <william.mccumstie@outlook.com>
2018-05-12 15:53:41 +0100

    Require the Domain to have a filled name

--- lib/cloudware/models/domain.rb
+++ lib/cloudware/models/domain.rb
@@ -3,6 +3,7 @@ module Cloudware
   module Models
     class Domain < Application
       attr_accessor :name
+      validates_presence_of :name
     end
   end
 end
--- spec/cloudware/models/domain_spec.rb
+++ spec/cloudware/models/domain_spec.rb
@@ -3,4 +3,8 @@ RSpec.describe Cloudware::Models::Domain do
   it 'can build a valid domain object' do
     expect(build(:domain)).to be_valid
   end
+
+  it 'requires the name field' do
+    expect(build(:domain, name: nil)).not_to be_valid
+  end
 end
