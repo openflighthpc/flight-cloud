8b7202d48a82969693d83b9d795b2b07836a113f
william <william.mccumstie@outlook.com>
2018-05-15 12:36:54 +0100

    Define a method for finding domains by region
    The old way of finding the domains required polling all the regions.
    This is inefficient if the user has a single region in mind.
    
    Instead the code has been adapted into a new `domains_by_region` method.
    This way a single region can be polled for its domains

--- lib/cloudware/providers/domains/aws.rb
+++ lib/cloudware/providers/domains/aws.rb
@@ -3,6 +3,39 @@ module Cloudware
   module Providers
     module Domains
       class AWS < Domain
+        class << self
+          def by_region(region)
+            ec2_by_region(region).describe_vpcs(
+              filters: [{ name: 'tag-key', values: ['cloudware_id'] }]
+            ).vpcs.map { |vpc| build_domain(region, vpc) }
+          end
+
+          private
+
+          def ec2_by_region(region)
+            Aws::EC2::Client.new(
+              region: region,
+              credentials: Cloudware.config.credentials.aws
+            )
+          end
+
+          def build_domain(region, vpc)
+            args = { provider: 'aws', region: region }
+            Models::Domain.build(**args).tap do |domain|
+              vpc.tags.each do |tag|
+                case tag.key
+                when 'cloudware_domain'
+                  domain.name = tag.value
+                when 'cloudware_network_cidr'
+                  domain.networkcidr = tag.value
+                when 'cloudware_pri_subnet_ip'
+                  domain.prisubnetcidr = tag.value
+                end
+              end
+            end
+          end
+        end
+
         include DeployAWS
 
         def create
