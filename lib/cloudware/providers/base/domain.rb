b2a678c6570f504df67ee19c4d91a5bb7f0a8181
william <william.mccumstie@outlook.com>
2018-05-15 16:54:08 +0100

    Add the base methods to Base::Domain
    This makes it clear which methods need to be overridden

--- lib/cloudware/providers/base/domain.rb
+++ lib/cloudware/providers/base/domain.rb
@@ -8,6 +8,14 @@ module Cloudware
           @domain_model = domain_model
         end
 
+        def create
+          raise NotImplementedError
+        end
+
+        def destroy
+          raise NotImplementedError
+        end
+
         private
 
         attr_reader :domain_model
