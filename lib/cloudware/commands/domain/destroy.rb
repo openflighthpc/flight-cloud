069f45f6722e0b0cc2d1ca1afe8c0c31b3223df7
william <william.mccumstie@outlook.com>
2018-05-15 14:29:15 +0100

    Switch checking if a domain exists to the new model
    Only domains that are created by `cloudware` should be deleted by
    `cloudware`. As such all the domains are belong to the region are polled
    and then checked for the input name.
    
    If the domain does not exist, then it errors. This is to prevent
    cloudware deleting domains it does not manage.
    
    Currently the original domain object still does the deletion

--- lib/cloudware/commands/domain/destroy.rb
+++ lib/cloudware/commands/domain/destroy.rb
@@ -10,13 +10,23 @@ module Cloudware
           d_remove_me = Cloudware::Domain.new
           d_remove_me.name = name
 
-          run_whirly('Checking domain exists') do
-            raise("Domain name #{options.name} does not exist") unless d_remove_me.exists?
-          end
-
+          run_whirly('Checking domain exists') { domain }
           run_whirly("Destroying domain #{options.name}") do
             d_remove_me.destroy
           end
+        end
+
+        private
+
+        def domain
+          model = domains_in_region.find { |d| d.name == name }
+          return model unless model.nil?
+          raise InvalidInput, "Domain name '#{name}' does not exist"
+        end
+
+        def domains_in_region
+          Providers::Domains.by_provider(options.provider)
+                            .by_region(options.region)
         end
       end
     end
--- lib/cloudware/providers/domains/domain.rb
+++ lib/cloudware/providers/domains/domain.rb
@@ -2,7 +2,24 @@
 module Cloudware
   module Providers
     module Domains
+      class << self
+        def by_provider(provider)
+          case provider
+          when 'aws'
+            AWS
+          when 'azure'
+            raise NotImplementedError
+          end
+        end
+      end
+
       class Domain
+        class << self
+          def by_region(_r)
+            raise NotImplementedError
+          end
+        end
+
         def initialize(domain_model)
           @domain_model = domain_model
         end
