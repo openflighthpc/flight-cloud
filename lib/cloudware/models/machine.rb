3b5d3377b05721fd5df022b8ed5cd6aa91fd3db0
william <william.mccumstie@outlook.com>
2018-05-16 15:59:49 +0100

    Delegate the `provider`, `region` to the domain
    The machine must be in the same region as the domain. However this means
    it has two sets of `attributes` the Provider modules will need to
    access.
    
    Thus a combined `delegate_attributes` has been defined

--- lib/cloudware/models/machine.rb
+++ lib/cloudware/models/machine.rb
@@ -3,7 +3,23 @@ module Cloudware
   module Models
     class Machine < Application
       ATTRIBUTES = [:name, :type, :flavour, :domain, :role, :priip]
+      DOMAIN_ATTRIBUTES = [:region, :provider]
+
+      def self.delegate_attributes
+        ATTRIBUTES.dup.concat(DOMAIN_ATTRIBUTES.dup)
+      end
+
       attr_accessor(*ATTRIBUTES)
+      delegate(*DOMAIN_ATTRIBUTES, to: :domain)
+
+      def run_create
+      end
+
+      private
+
+      def provider_module
+        Providers.select(provider)
+      end
     end
   end
 end
