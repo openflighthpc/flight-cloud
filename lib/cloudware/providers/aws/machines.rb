58c16463c6bd609225820688c8b384fa270ce684
william <william.mccumstie@outlook.com>
2018-05-17 13:16:46 +0100

    Define the AWS version of the Machines `class`

---
+++ lib/cloudware/providers/aws/machines.rb
@@ -0,0 +1,33 @@
+
+# frozen_string_literal: true
+
+module Cloudware
+  module Providers
+    module AWS
+      class Machines < Base::Domains
+        class Builder
+          def initialize(region)
+            @region ||= region
+            @ec2 = Aws::EC2::Client.new(
+              region: region,
+              credentials: Cloudware.config.credentials.aws
+            )
+          end
+
+          def models
+          end
+
+          private
+
+          attr_reader :region, :ec2
+
+          def tags_structs(tags_struct)
+            OpenStruct.new(
+              tags_struct.map { |t| [t.key, t.value] }.to_h
+            )
+          end
+        end
+      end
+    end
+  end
+end
