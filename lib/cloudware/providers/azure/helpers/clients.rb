8aaf98eb69b3528a82d1e32a9623b511db3d64a6
william <william.mccumstie@outlook.com>
2018-06-04 12:23:03 +0100

    Add the network client to `AzureClient` class
    This way it can be easily accessed from other locations. Also the
    `Memoist` gem has been used to clean up the class

--- lib/cloudware/providers/azure/helpers/clients.rb
+++ lib/cloudware/providers/azure/helpers/clients.rb
@@ -4,13 +4,21 @@ module Cloudware
     module AZURE
       module Helpers
         class AzureClient
+          extend Memoist
+
           def resource
-            @resource ||= begin
-              Azure::Resources::Profiles::Latest::Mgmt::Client.new(
-                Cloudware.config.credentials.azure
-              )
-            end
+            Azure::Resources::Profiles::Latest::Mgmt::Client.new(
+              Cloudware.config.credentials.azure
+            )
           end
+          memoize :resource
+
+          def network
+            Azure::Network::Profiles::Latest::Mgmt::Client.new(
+              Cloudware.config.credentials.azure
+            )
+          end
+          memoize :network
         end
 
         module Client
