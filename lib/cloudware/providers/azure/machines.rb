56226bdf0f603fcd5c64382a5ee61cff96c4b5a7
william <william.mccumstie@outlook.com>
2018-06-04 13:19:48 +0100

    Save the external ip into the model
    The downloaded list of public ips can then be matched to the specific
    machine. Note that the `ips` are first filtered by resource group encase
    there are two machines with the same name but in different domains.

--- lib/cloudware/providers/azure/machines.rb
+++ lib/cloudware/providers/azure/machines.rb
@@ -49,15 +49,26 @@ module Cloudware
             domain = domains.find do |d|
               d.resource_group.name == tags.cloudware_domain
             end
+            name = tags.cloudware_machine_name
+            public_ip = find_public_ip(domain, name)
             Machine.build(
               domain: domain,
-              name: tags.cloudware_machine_name,
+              name: name,
               id: tags.cloudware_id,
               role: tags.cloudware_machine_role,
               priip: tags.cloudware_pri_ip,
+              extip: public_ip.ip_address,
               provider_type: tags.cloudware_machine_type,
               flavour: tags.cloudware_machine_flavour
             )
+          end
+
+          def find_public_ip(domain, machine_name)
+            public_ipaddresses.find do |ip|
+              rg_name = domain.resource_group.name
+              next unless ip.tags['cloudware_domain'] == rg_name
+              ip.name == machine_name
+            end
           end
         end
       end
