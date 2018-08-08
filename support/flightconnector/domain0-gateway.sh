904f5603c6de0c51ba6ce117b13683e0e168004e
stuts <stu.j.franks@gmail.com>
2018-08-08 10:03:54 +0100

    Add additional DNS zones

--- support/flightconnector/domain0-gateway.sh
+++ support/flightconnector/domain0-gateway.sh
@@ -38,6 +38,7 @@ everyware_IPA_PASSWORD="${everyware_IPA_PASSWORD:-REPLACE_ME}"
 everyware_IPA_HOST="${everyware_IPA_HOST:-$(hostname -f)}"
 everyware_IPA_HOSTIP="${everyware_IPA_HOSTIP:-$(gethostip -d $(hostname))}"
 everyware_IPA_REALM="${everyware_IPA_REALM:-$(hostname -d |sed 's/^[^.]*.//g' |tr '[a-z]' '[A-Z]')}"
+everyware_IPA_REALM_DOWNCASE="${everyware_IPA_REALM:-$(hostname -d |sed 's/^[^.]*.//g')}"
 everyware_IPA_DOMAIN="${everyware_IPA_DOMAIN:-$(hostname -d)}"
 everyware_IPA_DNS="${everyware_IPA_DNS:-$(grep nameserver -m 1 /etc/resolv.conf  |sed 's/nameserver //g')}"
 everyware_IPA_REVERSE="${everyware_IPA_REVERSE:-$(gethostip -d $(hostname) |awk -F. '{print $2"."$1}')}"
@@ -401,5 +402,10 @@ systemctl restart dbus # fix for certmonger error https://bugzilla.redhat.com/sh
 
 ipa-server-install -a $everyware_IPA_PASSWORD --hostname $everyware_IPA_HOST --ip-address=$everyware_IPA_HOSTIP -r "$everyware_IPA_REALM" -p $everyware_IPA_PASSWORD -n "$everyware_IPA_DOMAIN" --no-ntp --setup-dns --forwarder="$everyware_IPA_DNS" --reverse-zone="$everyware_IPA_REVERSE.in-addr.arpa." --ssh-trust-dns --unattended
 
+echo $everyware_IPA_PASSWORD |kinit admin
 
-echo "Please reboot"
+ipa dnszone-add cluster1.$everyware_IPA_REALM_DOWNCASE --dynamic-update=true
+ipa dnszone-add cluster2.$everyware_IPA_REALM_DOWNCASE --dynamic-update=true
+ipa dnszone-add cluster3.$everyware_IPA_REALM_DOWNCASE --dynamic-update=true
+ipa dnszone-add cluster4.$everyware_IPA_REALM_DOWNCASE --dynamic-update=true
+ipa dnszone-add 100.10.in-addr.arpa. --dynamic-update=true
