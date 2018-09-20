e983729e370cf3945fe2dfcd303552fe0a548bbf
stuts <stu.j.franks@gmail.com>
2018-09-20 15:52:38 +0100

    Tweak vars and add check

--- support/flightblueprint/blueprint_to_cluster.rb
+++ support/flightblueprint/blueprint_to_cluster.rb
@@ -3,6 +3,11 @@ require 'yaml'
 
 input_file = ARGV[0]
 
+if input_file.nil?
+  puts "The first argument must be a correct path to a flight blueprint yaml file"
+  exit 1
+end
+
 unless File.file?(input_file)
   puts "The first argument must be a correct path to a flight blueprint yaml file"
   exit 1
@@ -14,10 +19,10 @@ all_clusters = data.tap { |hash| hash.delete('core')}
 all_clusters.each do |cluster, groups|
   ENV['CLUSTER_NAME'] = cluster
   ENV['LOGIN_NODE'] = "#{cluster}-#{groups['login']}"
-  ENV['GROUPS'] = groups['compute'].keys.join(' ')
+  ENV['CLUSTER_GROUPS'] = groups['compute'].keys.join(' ')
   groups['compute'].each do |group, nodes|
     ENV["#{group}_NODES"] = nodes['nodes'].map{|n| "#{cluster}-#{n}"}.join(" ")
-    ENV["#{group}_SEC_GROUPS"] = nodes['secondaryGroups'].join(' ')
+    ENV["#{group}_SEC_GROUPS"] = nodes['secondaryGroups'].delete_if{|s|s=='all'}.join(' ')
   end
   
   puts `bash /root/add-cluster.sh`
