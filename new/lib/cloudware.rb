d5112e331ae4d115f4d12d783134858a9fb0d343
Bob Whitelock <bob.whitelock1@gmail.com>
2018-11-15 19:00:43 +0000

    Customize generated Gem for writing app
    - Switch out `gemspec` for `Gemfile` as not writing a Gem;
    - set up automatic requiring of all Gems and autoloading of all app code;
    - remove generated README;
    - remove generated failing test.

--- new/lib/cloudware.rb
+++ new/lib/cloudware.rb
@@ -1,5 +1,11 @@
-require "cloudware/version"
+
+# Set up Bundler, require all Gems, and then autoload everything under
+# `lib/cloudware` (so don't need to inconsistently litter `require`s
+# everywhere; instead, so long as things are defined in the correct, consistent
+# directory structure, everything will just be autoloaded when used).
+require 'bundler/setup'
+Bundler.require
+autoload_all 'lib/cloudware'
 
 module Cloudware
-  # Your code goes here...
 end
