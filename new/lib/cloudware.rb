f8b094b8a462e719c5efff86069144196be22d3b
Bob Whitelock <bob.whitelock1@gmail.com>
2018-11-16 19:21:36 +0000

    Add note on debugging auto-loading

--- new/lib/cloudware.rb
+++ new/lib/cloudware.rb
@@ -2,7 +2,8 @@
 # Set up Bundler, require always needed Gems, and then autoload everything
 # under `lib/cloudware` (so don't need to inconsistently litter `require`s
 # everywhere; instead, so long as things are defined in the correct, consistent
-# directory structure, everything will just be autoloaded when used).
+# directory structure, everything will just be autoloaded when used). Note:
+# `$DEBUG = true` can be used to debug auto-loading.
 require 'bundler/setup'
 
 require 'require_all'
