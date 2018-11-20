189d6baf5a23e31a8a98904e5e4aa5f30b79c0c3
william <william.mccumstie@outlook.com>
2018-11-20 16:53:44 +0000

    Deploy the template to AWS
    The code has been rejigged a bit to use an instance as the client is
    region specific. This still needs to be hooked up to the CLI
    
    The template is then sent off to AWS for deployment

--- lib/cloudware/models/deployment.rb
+++ lib/cloudware/models/deployment.rb
@@ -8,6 +8,15 @@ module Cloudware
     class Deployment < Application
       attr_accessor :name, :provider
 
+      def tag_name
+        "cloudware-deploy-#{name}"
+      end
+
+      # TODO: Set this in the same way as the original version
+      def region
+        'eu-west-1'
+      end
+
       def path
         ext = (provider == 'aws' ? '.yaml' : '.json')
         File.join('/var/lib/cloudware/templates', provider, name) + ext
@@ -18,7 +27,7 @@ module Cloudware
       end
 
       def deploy
-        Providers::AWS.deploy(template)
+        Providers::AWS.new(region).deploy(tag_name, template)
       end
     end
   end
--- lib/cloudware/providers/AWS.rb
+++ lib/cloudware/providers/AWS.rb
@@ -3,14 +3,28 @@
 module Cloudware
   module Providers
     class AWS
-      class << self
-        attr_reader :credentials
+      extend Memoist
 
-        def deploy(template)
-        end
+      attr_reader :region
+
+      def initialize(region)
+        @region = region
       end
 
-      @credentials = Config.credentials.aws
+      def deploy(name, template)
+        client.create_stack(stack_name: name, template_body: template)
+        client.wait_until(:stack_create_complete, stack_name: name)
+      end
+
+      private
+
+      def client
+        Aws::CloudFormation::Client.new(
+          region: region,
+          credentials: Config.credentials.aws
+        )
+      end
+      memoize :client
     end
   end
 end
