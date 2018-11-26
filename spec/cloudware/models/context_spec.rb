4e889bf92bfe3be5f988f4f94cc3af51427a6b01
william <william.mccumstie@outlook.com>
2018-11-26 11:47:03 +0000

    Add a test to ensure the results are merged correctly
    This spec has been implicitly implemented already, however it is being
    tested explicitly to check for regressions

--- spec/cloudware/models/context_spec.rb
+++ spec/cloudware/models/context_spec.rb
@@ -19,4 +19,18 @@ RSpec.describe Cloudware::Models::Context do
       end
     end
   end
+
+  context 'with multiple deployments' do
+    let(:initial_results) { { key: 'value', replaced_key: 'wrong' } }
+    let(:final_results) { { replaced_key: 'correct' } }
+    let(:merged_results) { initial_results.merge(final_results) }
+
+    let(:initial_deployment) { build(:deployment, results: initial_results) }
+    let(:final_deployment) { build(:deployment, results: final_results) }
+    let(:deployments) { [initial_deployment, final_deployment] }
+
+    it 'replaces the earlier results with the latter' do
+      expect(subject.results).to eq(merged_results)
+    end
+  end
 end
