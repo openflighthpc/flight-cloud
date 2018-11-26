a6de06173ea07cfa5eb6fb92233119104b22ef6c
william <william.mccumstie@outlook.com>
2018-11-26 12:00:15 +0000

    Add a default value to the load method
    This allows it to load empty `YAML` files to be loaded

--- lib/cloudware/data.rb
+++ lib/cloudware/data.rb
@@ -5,13 +5,17 @@ require 'fileutils'
 
 module Cloudware
   class Data
+    DEFAULT_VALUE = {}
+
     class << self
-      def load(file)
-        load_string(File.read(file))
+      def load(file, default_value: DEFAULT_VALUE)
+        str = File.exists?(file) ? File.read(file) : ''
+        load_string(str, default_value: default_value)
       end
 
-      def load_string(string)
-        YAML.load(string).deep_symbolize_keys
+      def load_string(string, default_value: DEFAULT_VALUE)
+        raw = YAML.load(string).deep_symbolize_keys
+        raw.nil? ? default_value : raw
       end
 
       def dump(file, data)
