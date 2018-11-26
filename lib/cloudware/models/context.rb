694018e7588641a912aff7d5860c909e2d075c0b
william <william.mccumstie@outlook.com>
2018-11-26 15:56:32 +0000

    Replace the `stack` method with `deployments`
    The stack is no longer required as the internal data object has been
    removed. The deployments array can be used instead

--- lib/cloudware/models/context.rb
+++ lib/cloudware/models/context.rb
@@ -4,43 +4,39 @@ module Cloudware
   module Models
     class Context < Application
       def deployments
-        stack
+        @deployments ||= Data.load(path, default_value: []).map do |data|
+          Deployment.new(**data)
+        end
       end
 
       def deployments=(input_deployments)
-        @stack = [] # Reset the cache
+        @deployments = [] # Reset the cache
         input_deployments.each do |deployment|
           with_deployment(deployment)
         end
       end
 
       def results
-        stack.map(&:results)
+        deployments.map(&:results)
              .each_with_object({}) do |results, memo|
           memo.merge!(results || {})
         end
       end
 
       def with_deployment(deployment)
-        stack.push(deployment)
+        deployments.push(deployment)
       end
 
       def remove_deployment(deployment)
-        stack.delete_if { |d| d.name == deployment.name }
+        deployments.delete_if { |d| d.name == deployment.name }
       end
 
       def save
-        save_data = stack.map(&:to_h)
+        save_data = deployments.map(&:to_h)
         Data.dump(path, save_data)
       end
 
       private
-
-      def stack
-        @stack ||= Data.load(path, default_value: []).map do |data|
-          Deployment.new(**data)
-        end
-      end
 
       def path
         File.join(Config.content_path, 'contexts/default.yaml')
