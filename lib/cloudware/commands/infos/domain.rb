32883cae3082924cc412e771497c2d96e2d0121e
william <william.mccumstie@outlook.com>
2018-11-29 17:21:38 +0000

    Generalize the info methods
    These methods are broadly the same, the only difference being the model
    object that is being displayed

--- lib/cloudware/commands/infos/domain.rb
+++ lib/cloudware/commands/infos/domain.rb
@@ -5,27 +5,9 @@ require 'tty-table'
 module Cloudware
   module Commands
     module Infos
-      class Domain < Command
-        include Concerns::ExistingDeployment
-        include Concerns::Table
-        attr_reader :domain_name, :deployment_name
-
-        def run
-          @deployment_name = options.deployment
-          @domain_name = argv[0]
-          machine.tags.each { |row| table << row }
-          puts render_table
-        end
-
-        private
-
-        def machine
-          Models::Domain.new(name: domain_name, deployment: deployment)
-        end
-        memoize :machine
-
-        def table_header
-          ['Tag', 'Value']
+      class Domain < Info
+        def model_class
+          Models::Domain
         end
       end
     end
--- lib/cloudware/commands/infos/machine.rb
+++ lib/cloudware/commands/infos/machine.rb
@@ -5,27 +5,9 @@ require 'tty-table'
 module Cloudware
   module Commands
     module Infos
-      class Machine < Command
-        include Concerns::ExistingDeployment
-        include Concerns::Table
-        attr_reader :machine_name, :deployment_name
-
-        def run
-          @deployment_name = options.deployment
-          @machine_name = argv[0]
-          machine.tags.each { |row| table << row }
-          puts render_table
-        end
-
-        private
-
-        def machine
-          Models::Machine.new(name: machine_name, deployment: deployment)
-        end
-        memoize :machine
-
-        def table_header
-          ['Tag', 'Value']
+      class Machine < Info
+        def model_class
+          Models::Machine
         end
       end
     end
