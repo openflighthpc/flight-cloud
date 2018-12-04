9b78c5c3ed5c4eaafb3795bab30af5b0371072b8
william <william.mccumstie@outlook.com>
2018-12-04 11:19:05 +0000

    Port the code for creating azure deployments
    This is based on the code from the original version of cloudware. Each
    deployment will exist within its own resource group. This makes it more
    consistent with aws

--- lib/cloudware/providers/AZURE.rb
+++ lib/cloudware/providers/AZURE.rb
@@ -9,6 +9,37 @@ module Cloudware
       end
 
       class Client < Base::Client
+        def deploy(name, template)
+          group = create_resource_group(name)
+          deployment = build_deployment(template)
+          resource_client.deployments
+                         .create_or_update(group.name, name, deployment)
+        end
+
+        private
+
+        def create_resource_group(name)
+          rg_name = "#{name}-rg"
+          group = resource_client.model_classes.resource_group.new
+          group.location = region
+          resource_client.resource_groups.create_or_update(rg_name, group)
+        end
+
+        def build_deployment(template)
+          resource_client.model_classes.deployment.new.tap do |d|
+            d.properties = resource_client.model_classes.deployment_properties
+                                          .new.tap do |p|
+              p.template = JSON.parse(template)
+              p.mode = Azure::Resources::Profiles::Latest::Mgmt::Models::DeploymentMode::Complete
+            end
+          end
+        end
+
+        def resource_client
+          mod = Azure::Resources::Profiles::Latest::Mgmt::Client
+          mod.new(Config.credentials.azure)
+        end
+        memoize :resource_client
       end
     end
   end
