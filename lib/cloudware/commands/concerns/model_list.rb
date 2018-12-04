15e4894419d6da48a3c6d7f7a29b211eb34ea448
william <william.mccumstie@outlook.com>
2018-12-04 17:26:44 +0000

    Move the List command code to a concern
    The list commands are not well suited to inheritance as it forms to
    rigid of a structure.
    
    Instead the code has been moved to a concern as a composition adds
    greater flexibility

---
+++ lib/cloudware/commands/concerns/model_list.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'commands/concerns/table'
+
+module Cloudware
+  module Commands
+    module Concerns
+      module ModelList
+        extend Memoist
+        include Concerns::Table
+
+        def run
+          add_rows
+          puts render_table
+        end
+
+        private
+
+        def deployment_method
+          raise NotImplementedError
+        end
+
+        def models
+          Models::Context.new
+                         .deployments
+                         .map(&deployment_method)
+                         .flatten
+        end
+        memoize :models
+
+        def header_tags
+          models.map { |m| m.tags.keys }
+                  .flatten
+                  .uniq
+        end
+        memoize :header_tags
+
+        def table_header
+          [deployment_method.to_s.capitalize, 'Deployment', *header_tags]
+        end
+
+        def add_rows
+          models.each do |machine|
+            tags = machine.tags
+            machine_values = header_tags.map { |k| tags[k] }
+            table << [machine.name, machine.deployment.name, *machine_values]
+          end
+        end
+      end
+    end
+  end
+end
