cbb6f947bafe138fd608d56d746704ae0ac8f6e7
william <william.mccumstie@outlook.com>
2018-12-13 11:21:41 +0000

    Remove the inheritance on TTY::Spinner
    Inheritance is a poor design choice here as it could lead to conflicts
    in naming
    
    Instead the `Spinner` class creates and runs an internal `TTY::Spinner`
    
    It also means `run_with_background_checks` can be renamed `run`

--- lib/cloudware/spinner.rb
+++ lib/cloudware/spinner.rb
@@ -3,11 +3,19 @@ require 'tty-spinner'
 
 module Cloudware
   class Spinner < TTY::Spinner
-    def run_with_background_checks(&block)
+    def initialize(*a, **k)
+      @tty_spinner = TTY::Spinner.new(*a, **k)
+    end
+
+    def run(&block)
       results = nil
-      run { |_| results = yield }
+      tty_spinner.run { |_| results = yield }
       results
     end
+
+    private
+
+    attr_reader :tty_spinner
   end
 
   module WithSpinner
