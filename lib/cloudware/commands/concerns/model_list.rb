b36e7c6fd33856b6c37472de3e3409add1a202ec
william <william.mccumstie@outlook.com>
2018-12-14 11:23:01 +0000

    Remove the deployment from machine list
    Assigning the deployment to machines no longer makes sense as machines
    are built from the context instead

--- lib/cloudware/commands/concerns/model_list.rb
+++ lib/cloudware/commands/concerns/model_list.rb
@@ -35,14 +35,14 @@ module Cloudware
         memoize :header_tags
 
         def table_header
-          [deployment_method.to_s.capitalize, 'Deployment', *header_tags]
+          [deployment_method.to_s.capitalize, *header_tags]
         end
 
         def add_rows
           models.each do |machine|
             tags = machine.tags
             machine_values = header_tags.map { |k| tags[k] }
-            table << [machine.name, machine.deployment.name, *machine_values]
+            table << [machine.name, *machine_values]
           end
         end
       end
