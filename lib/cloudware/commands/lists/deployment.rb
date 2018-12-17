07ed55b63bd18c0d157447c03d58a642e7a8443f
william <william.mccumstie@outlook.com>
2018-12-17 12:02:36 +0000

    Remove the old table run methods
    This is now redundant as the table is being replaced with markdown

--- lib/cloudware/commands/lists/deployment.rb
+++ lib/cloudware/commands/lists/deployment.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-require 'stringio'
-require 'pp'
-
 module Cloudware
   module Commands
     module Lists
       class Deployment < Command
-        include Concerns::Table
 
         TEMPLATE = <<-TEMPLATE
 <% deployments.each do |deployment| -%>
@@ -29,35 +25,6 @@ template: <%= deployment.template_path %>
 TEMPLATE
 
         def run
-          context.deployments.each do |deployment|
-            table << generate_row(deployment)
-          end
-          page_table
-        end
-
-        def table_attributes
-          {
-            'Deployment' => Proc.new { |d| d.name },
-            'Results' => Proc.new { |d| pretty_to_s(d.results) },
-            'Replacements' => Proc.new { |d| pretty_to_s(d.replacements) }
-          }
-        end
-
-        def table_header
-          table_attributes.keys
-        end
-
-        def generate_row(deployment)
-          table_attributes.values.map do |value_proc|
-            value_proc.call(deployment)
-          end
-        end
-
-        def pretty_to_s(obj)
-          io = StringIO.new
-          PP.pp(obj, io)
-          io.rewind
-          io.read
         end
       end
     end
