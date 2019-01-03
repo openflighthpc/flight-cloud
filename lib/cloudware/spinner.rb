cd8fa10c7d5189cd193668dc3a511a9f9428386a
william <william.mccumstie@outlook.com>
2019-01-03 16:46:49 +0000

    Extract the counter into its own method
    The count is only used to determine if the foreground status should be
    checked. As such it safe to do the incrementing within the method itself
    
    If the class is reused, then the count will still without restarting

--- lib/cloudware/spinner.rb
+++ lib/cloudware/spinner.rb
@@ -38,10 +38,8 @@ module Cloudware
     def run(done_message = '')
       results = nil
       thr = Thread.new { results = yield }
-      count = 0
       until thr.join(SPIN_DELAY)
         update_foreground_status if (count % CHECK_SPIN).zero?
-        count += 1
         tty_spinner.spin if spin?
       end
       results
@@ -53,6 +51,11 @@ module Cloudware
 
     attr_reader :tty_spinner
     attr_accessor :foreground
+
+    def count
+      @count ||= -1
+      @count += 1
+    end
 
     def update_foreground_status
       status = `ps -o stat= -p #{Process.pid}`
