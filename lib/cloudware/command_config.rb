2ce91f610bdcb817e8b478fe774128209ca18e5d
stuts <stu.j.franks@gmail.com>
2019-04-02 12:41:29 +0100

    Update license header

--- lib/cloudware/command_config.rb
+++ lib/cloudware/command_config.rb
@@ -1,28 +1,31 @@
 # frozen_string_literal: true
 
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
 #
-# =============================================================================
-# Copyright (C) 2019 Stephen F. Norledge and Alces Flight Ltd
+# This file is part of Flight Cloud.
 #
-# This file is part of Alces Cloudware.
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
 #
-# Alces Cloudware is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# Flight Cloud is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
 #
-# Alces Cloudware is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Cloud. If not, see:
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with Alces Cloudware.  If not, see <http://www.gnu.org/licenses/>.
+#  https://opensource.org/licenses/EPL-2.0
 #
-# For more information on the Alces Cloudware, please visit:
-# https://github.com/alces-software/cloudware
-# ==============================================================================
-#
+# For more information on Flight Cloud, please visit:
+# https://github.com/openflighthpc/flight-cloud
+#===============================================================================
 
 require 'cloudware/config'
 require 'active_support/core_ext/module/delegation'
@@ -59,4 +62,3 @@ module Cloudware
     end
   end
 end
-
