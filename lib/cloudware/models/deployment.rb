3498b8575be85a4300962bf35512a21182f38318
Andrew <andrew.toghill@alces-software.com>
2019-06-21 11:29:39 +0100

    Resolve key value pairs as necessary

--- lib/cloudware/models/deployment.rb
+++ lib/cloudware/models/deployment.rb
@@ -155,7 +155,11 @@ module Cloudware
 
       def template
         return raw_template unless replacements
+        dep = ReplacementFactory.new(cluster, self.name)
         replacements.reduce(raw_template) do |memo, (key, value)|
+          # Resolve domain(s) of key value pairs if necessary
+          value = dep.parse_key_pair(key.to_sym, value) if value.include? "*"
+
           memo.gsub("%#{key}%", value.to_s)
         end
       end
