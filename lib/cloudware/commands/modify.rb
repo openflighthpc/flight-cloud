e82ef26d76254cb90fddaac96e8463603f2a9c74
Andrew <andrew.toghill@alces-software.com>
2019-08-02 11:11:28 +0100

    Initial commit

---
+++ lib/cloudware/commands/modify.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+#==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of Flight Cloud.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Cloud is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Cloud. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Cloud, please visit:
+# https://github.com/openflighthpc/flight-cloud
+#===============================================================================
+
+module Cloudware
+  module Commands
+    class Modify < Command
+      def run!(node, instance_type)
+      end
+    end
+  end
+end
